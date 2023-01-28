ed8ada5b201ea3a0698171c000b8f40f17f1de50
S4nfs <zyzzaestheticsgods@gmail.com>
2023-01-28 23:18:05 +0530

    debug code

--- debug.js
+++ debug.js
@@ -1,26 +1,15 @@
-
-
-function migratoryBirds(arr) {
-    arr = arr.sort((a, b) => a - b);
-    let result = [0, 0];
-    for (let i = 0; i < arr.length; i++) {
-        let count = 1
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] == arr[j]) {
-                count += 1
-            }
+function gradingStudents(grades) {
+    let num = []
+    for (let g = 0; g < grades.length; g++) {
+        let nextNum = Math.ceil(grades[g] / 5) * 5
+        if ((grades[g] >= 38) && ((nextNum - grades[g]) < 3)) {
+            num[num.length] = nextNum //or num = [...num, nextNum]  or num.splice(num, 0 , nextNum)
+        } else {
+            num[num.length] = grades[g]
         }
-
-        if (count > result[0]) {
-            result[0] = count;
-            result[1] = arr[i]
-        }
-
     }
-    return result[1]
+    return num
 
 }
 
-console.log(migratoryBirds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]))
-
-// [1, 4 ,4 ,4 ,5 ,3]
+console.log(gradingStudents([73, 67, 38, 33]))
--- debug2.js
+++ debug2.js
@@ -1,20 +1,16 @@
-function migratoryBirds(arr) {
-    arr = arr.sort((a, b) => a - b);
-    const l = arr.length;
-    let result = [0, 0]; // [0] => type, [1] => count birds
-    for (let i = 0; i < l; i++) {
-        let count = 1;
-        for (let j = i + 1; j < l; j++) {
-            if (arr[i] === arr[j]) {
-                count += 1;
+function climbingLeaderboard(ranked, player) {
+    let sortRanked = ranked.sort((a, b) => a - b) // [60,75,80,90,90,100]
+    let rank = ranked.length + 1
+    for (let i = 0; i < player.length; i++) {
+        let position
+        for (let j = 0; j < sortRanked.length; j++) {
+            if ((player[i] < sortRanked[j] && j == 0) || (player[i] >= sortRanked[j] && player[i] < sortRanked[j + 1]) || (player[i] > sortRanked[sortRanked.length - 1])) {
+                rank--
+                position = rank
             }
         }
-        if (count > result[1]) {
-            result[1] = count;
-            result[0] = arr[i];
-        }
+        console.log(rank)
     }
-    return result[0];
 }
 
-console.log(migratoryBirds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]))
+console.log(climbingLeaderboard([100, 90, 90, 80, 75, 60], [50, 65, 77, 90, 102]))
