b3cf609172752dcdf2288719e419e124c0004395
S4nfs <zyzzaestheticsgods@gmail.com>
2023-02-01 22:57:31 +0530

    added enum and other data types

--- Typescript_Course/hello_Typescript/app.js
+++ Typescript_Course/hello_Typescript/app.js
@@ -1,5 +1,5 @@
 console.log("Hello Typescript");
-//cheking the types
+//✔️Cheking the types
 var add = function (n1, n2, result, str) {
     if (result) {
         var sum = n1 + n2; //avoid concatination
@@ -8,3 +8,28 @@ var add = function (n1, n2, result, str) {
 };
 add(30, 5.2, true, "Result is: ");
 //The key difference is: javascript uses "dynamic types" resolved at (runtime) whereas Typescript uses "static types" set during (development)
+//✔️Typescript work flawless when checking errors, repeat "flawless". Lets see other data types like touple
+var person = {
+    name: "Sagar Verma",
+    age: 23,
+    friends: [4, "pratik", "gaurav", "ritvik", "rohan"],
+    role: [2, "SWE"]
+};
+person.role.push(22);
+// person.role[1](22); //❌ will not throw error
+console.log(person);
+// for (let a of person.friends) {
+//     console.log(a.map((element) => element ))  //❌ will not throw error in normal js in build time
+// }
+//✔️An enum is a special "class" that represents a group of constants (unchangeable variables). Comes in two flavors string and numeric
+var Role;
+(function (Role) {
+    Role[Role["Enterprise"] = 0] = "Enterprise";
+    Role[Role["Admin"] = 44] = "Admin";
+    Role[Role["Moderator"] = 45] = "Moderator";
+    Role["Client"] = "client";
+})(Role || (Role = {}));
+console.log(Role.Enterprise); //0
+console.log(Role.Admin); //44
+console.log(Role.Moderator); //45
+console.log(Role.Client); //"client"
--- Typescript_Course/hello_Typescript/app.ts
+++ Typescript_Course/hello_Typescript/app.ts
@@ -1,6 +1,6 @@
 console.log("Hello Typescript");
 
-//cheking the types
+//✔️Cheking the types
 const add = (n1: number, n2: number, result: boolean, str: string) => {
   if (result) {
     let sum = n1 + n2; //avoid concatination
@@ -10,3 +10,36 @@ const add = (n1: number, n2: number, result: boolean, str: string) => {
 add(30, 5.2, true, "Result is: ");
 
 //The key difference is: javascript uses "dynamic types" resolved at (runtime) whereas Typescript uses "static types" set during (development)
+
+//✔️Typescript work flawless when checking errors, repeat "flawless". Lets see other data types like touple
+const person: {
+  name: string;
+  age: number;
+  friends: any[]; //array with any type
+  role: [number, string]; //touple - is a typed array with predefined length and type
+} = {
+  name: "Sagar Verma",
+  age: 23,
+  friends: [4, "pratik", "gaurav", "ritvik", "rohan"],
+  role: [2, "SWE"],
+};
+
+person.role.push(22);
+// person.role[1](22); //❌ will not throw error
+console.log(person);
+
+// for (let a of person.friends) {
+//     console.log(a.map((element) => element ))  //❌ will not throw error in normal js in build time
+// }
+
+//✔️An enum is a special "class" that represents a group of constants (unchangeable variables). Comes in two flavors string and numeric
+enum Role {
+  Enterprise,
+  Admin = 44, //numeric
+  Moderator,
+  Client = "client", //string
+}
+console.log(Role.Enterprise); //0
+console.log(Role.Admin); //44
+console.log(Role.Moderator); //45
+console.log(Role.Client); //"client"
