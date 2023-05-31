72f8f1c9f4245efa6fb3169be7d0a76764d178e4
S4nfs <zyzzaestheticsgods@gmail.com>
2023-05-31 23:23:06 +0530

    abstract class in-depth

--- Typescript_Course/hello_Typescript/3_abstractClass.ts
+++ Typescript_Course/hello_Typescript/3_abstractClass.ts
@@ -1,5 +1,9 @@
 /*✅ Abstraction means showing the essential features and hiding the background details. U can achieve abstraction by using:
-Abstract Class and Interface
+-Abstract Classes
+-Interfaces
+-Generics
+-Type Aliases
+
 
 Abstract Class - is a class that contains empty methods as well as non-empty methods, have constructor and variables, but instanciation is not possible (meaning u cant create an object of that class which is helpful when u want that class to be untouched).
 */
@@ -8,11 +12,11 @@ abstract class Department2 {
 }
 
 class QADepartment2 extends Department2 {
-  describe2(this: Department2): void {
+  describe2(this: Department2) {
     console.log("Hello from Abstract method");
   }
 }
 // const obj = new Department2(1, "ABC"); //❌ can't instantiate an abstract class
 
-const newObj2 = new QADepartment2(2, ["Guri", "Sonu"], []);
-newObj2.describe2;
+const newObj2 = new QADepartment2();
+newObj2.describe2();
