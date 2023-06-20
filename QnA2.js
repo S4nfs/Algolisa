cf46368543baf0ecb9b0e696082ce5bd80cf8a9c
S4nfs <zyzzaestheticsgods@gmail.com>
2023-06-20 21:52:13 +0530

    updated DSA ques

--- QnA2.js
+++ QnA2.js
@@ -122,3 +122,117 @@ console.log(findOldest([{ name: "liam", age: 22 },
 { name: "vishal", age: 20 },
 { name: "arnish", age: 29 },
 { name: "brian", age: 18 }]))
+
+
+
+
+/*===================================================================================================================
+ __  __      _       _
+|  \/  |__ _| |_ _ _(_)__ ___ ___
+| |\/| / _` |  _| '_| / _/ -_|_-<
+|_|  |_\__,_|\__|_| |_\__\___/__/
+
+/* Q3. Write a method that returns the frequency of the max integer in the matrix after increament by 1 within matrix operations? In the below example, it would be 1(since 2 shows up just once).
+
+Input:
+m=4
+n=4
+operations = [[1,1],[2,2]]
+
+Output= 1
+
+Explaination:
+[[1, 0, 0, 0],
+ [0, 0, 0, 0],
+ [0, 0, 0, 0]]
+
+[[2, 1, 0, 0],
+ [1, 1, 0, 0],
+ [0, 0, 0, 0]]  //max is 2 which occurs 1 times after overlapping
+*///=================================================================================================================
+function maxFromOps(m, n, operations) {
+    var minCol = m;
+    var minRow = n;
+
+    for (let op of operations) {
+        minCol = Math.min(minCol, op[0]);
+        minRow = Math.min(minRow, op[1]);
+    }
+    return minCol * minRow;
+}
+
+console.log(maxFromOps(4, 4, [
+    [1, 1],
+    [2, 2],
+]));
+
+
+//===================================================================================================================
+/* Q4. Given an integer array coins[ ] of size N representing different denominations of currency and an integer sum, find the number of ways you can make sum by using different combinations from coins[ ].  
+Note: Assume that you have an infinite supply of each type of coin. 
+
+Input:
+sum = 4 , 
+N = 3
+coins[] = {1,2,3}
+
+Output: 4
+Explanation: Four Possible ways are:
+{1,1,1,1},{1,1,2},{2,2},{1,3}.
+*///=================================================================================================================
+
+
+//===================================================================================================================
+/* Q5. In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.If the town judge exists, then:1. The town judge trusts nobody.
+2. Everybody (except for the town judge) trusts the town judge.
+3. There is exactly one person that satisfies properties 1 and 2.You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
+
+
+Example 1:
+
+Input: N = 2, trust = [[1,2]]
+Output: 2
+
+Example 2:
+
+Input: N = 3, trust = [[1,3],[2,3]]
+Output: 3
+
+Example 3:
+
+Input: N = 3, trust = [[1,3],[2,3],[3,1]]
+Output: -1
+
+Example 4:
+
+Input: N = 3, trust = [[1,2],[2,3]]
+Output: -1
+
+Example 5:
+
+Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+Output: 3
+*///=============================================================================================================================
+
+
+function findTownJudge(N, trust) {
+    // Create an array of counters to keep track of how many people trust each person
+    const trustCounts = new Array(N + 1).fill(0);
+
+    // Iterate over the trust pairs and update the counters
+    for (const [a, b] of trust) {
+        // console.log([a]) //[ 1 ][ 1 ][ 2 ][ 2 ][ 4 ]
+        console.log([a, b])
+        trustCounts[a]--;
+        trustCounts[b]++;
+    }
+    // Find the person with N-1 trusts (everyone trusts except the judge)
+    for (let i = 1; i <= N; i++) {
+        if (trustCounts[i] === N - 1) {
+            return i;
+        }
+    }
+    // No town judge found
+    return -1;
+}
+console.log(findTownJudge(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]]))
