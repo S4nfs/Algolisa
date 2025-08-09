d59c6160e7e31963e160b64862b9568541261afb
S4NFS <zyzzaestheticsgods@gmail.com>
2025-08-09 23:18:32 +0530

    feat: enhance Employee class with benefits and improve salary display, Added docker sommands

--- PYTHON/14_OOPs.py
+++ PYTHON/14_OOPs.py
@@ -4,56 +4,84 @@ class Employee:
     company = "Google"
     salary = "500"
 
+
 sagar = Employee()
 print(sagar.company)
-Employee.company = "Chatnaut"  # changing class attribute
+Employee.company = "Chatnaut"           # changing class attribute
 print(sagar.company)
 
 sagar.salary = 1000
-print(sagar.salary)  # changing instance attribute [limited to instance only]
+# changing instance attribute [limited to instance only]
+print(sagar.salary)
 
 
+class Benefits:
 
-# Self
-class Employee:
+    health_benefits = []
+
+    def __init__(self):
+        self.health_benefits = ["Health Insurance",
+                                "Dental Insurance", "Vision Insurance", "401(k)"]
+
+
+class Employee2:
     company = "Google"
+    CTC = 0
+
+    # constructor aka dunder (double underscore) method in python
+    def __init__(self, whatsTheCTC, name):  # Self (similar to "this" keyword in Javascript)
+        self.CTC = whatsTheCTC
+        self.name = name
+        self.benefits = []
+        # private attributes denoted by __ must be accessible withing the class
+        self.__employeeID = 544445554545
 
     def getSalary(self):
-        print(f"My salary in {self.company} is {self.salary}")
+        print(f"My salary at {self.company} is {self.salary}")
 
-sagar = Employee()
-sagar.salary = 1000
-sagar.getSalary()   #same as Employee.getSalary(sagar)
+    # The __str__() method controls what should be returned when the class object is represented as a string
+    def __str__(self):
+        return f"return as string {self.company} {self.salary}"
+
+    def getEmplooyeeID(self):
+        return self.__employeeID
 
 
-#Static Method - When we don't want to use self parameter
-class Employee:
-    company = "Google"
+employee2 = Employee2(8000000, "Sagar")
+employee2.salary = 10000000
+print("Name", employee2.name)
+employee2.getSalary()                       # same as Employee.getSalary(employee2)
+print(employee2)
+# if you want to see whats the instance looks like
+# The __str__() method convert the output of class to dictionary
+print("How it looks!", employee2.__dict__)
+print(employee2.CTC)
+employee2.benefits.append("401(k) Plan")
+print(employee2.benefits)
+print("Employee ID is: ", employee2.getEmplooyeeID())
+
+# Static Method - When we don't want to use self parameter
+
+
+class Employee3(Benefits):  # Inheriting the class Benefits
+    company = "Facebook"
+
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
 
     def getSalary(self):
-        print(f"My salary in {self.company} is {self.salary}")
+        print(f"My salary at {self.company} is {self.salary}")
 
     @staticmethod
     def greet():
         print("Good Morning Sir")
 
-sagar = Employee()
 
-sagar.salary = 1000
-sagar.getSalary()  
-sagar.greet()
+employee3 = Employee3("Rishabh")
 
-# 🍏-----------------------------------------------------------------------------------------------
-class RailwayForm:
-    formType = "RailwayForm"
-
-    def printData(self):  # method
-        print(
-            f"Traveller Name is {self.name} boarding from {self.train} as per {self.formType}")
-
-
-travellerApplication = RailwayForm()  # Object new instance
-
-travellerApplication.name = "Sagar"
-travellerApplication.train = "Gomti Express"
-travellerApplication.printData()
+employee3.salary = 10000
+employee3.getSalary()
+employee3.greet()
+print(employee3.name, "'s benefits at",
+      employee3.company, "are", employee3.health_benefits)
